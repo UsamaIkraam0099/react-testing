63f9e845a12da33be0c7c17c652a398c782f9779
UsamaIkraam0099 <usamaikraam0099@gmail.com>
2024-04-23 16:27:48 +0400

    find by done...

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -17,4 +17,34 @@ describe("Skills", () => {
     const listItemElement = screen.getAllByRole("list-item");
     expect(listItemElement).toHaveLength(skills.length);
   });
+
+  test("renders login button", () => {
+    render(<Skills skills={skills} />);
+
+    const loginButton = screen.getByRole("button", { name: "Login" });
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  test("start learning button is not rendered", () => {
+    render(<Skills skills={skills} />);
+
+    const startLearningBtn = screen.queryByRole("button", {
+      name: "Start Learning",
+    });
+    expect(startLearningBtn).not.toBeInTheDocument();
+  });
+
+  test("start learning button eventually displayed", async () => {
+    render(<Skills skills={skills} />);
+
+    const startLearningBtn = await screen.findByRole(
+      "button",
+      {
+        name: "Start Learning",
+      },
+      { timeout: 3000 } // default timeout 1000ms
+    );
+
+    expect(startLearningBtn).toBeInTheDocument();
+  });
 });
--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,25 +1,33 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { SkillsProps } from "./skills.types";
 
 export const Skills = (props: SkillsProps) => {
   const { skills } = props;
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setIsLoggedIn(true);
-  //   }, 1001);
-  // }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoggedIn(true);
+    }, 1001);
+  }, []);
 
   return (
-    <ul role="list">
-      {skills.map((skill) => {
-        return (
-          <li role="list-item" key={skill}>
-            {skill}
-          </li>
-        );
-      })}
-    </ul>
+    <Fragment>
+      <ul role="list">
+        {skills.map((skill) => {
+          return (
+            <li role="list-item" key={skill}>
+              {skill}
+            </li>
+          );
+        })}
+      </ul>
+
+      {isLoggedIn ? (
+        <button>Start Learning</button>
+      ) : (
+        <button onClick={() => setIsLoggedIn(true)}>Login</button>
+      )}
+    </Fragment>
   );
 };
